492ac8c9016a09af7d5b434e3b1f4297afa0f323
Guanliang Yan <guyan@ucsd.edu>
2016-01-21 13:58:29 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,40 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Hold on a sec...");
+		$(".jumbotron p").toggleClass("active");
+		$("#submitBtn").click(submit);
 	});
 
 	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	// example: $("#div-id").click(functionToCall);	
+	$("a.thumbnail").click(projectClick);
+}
+
+function submit(e){
+	var projectName = $("#project").val();
+	$(projectName).animate({width:$("#width").val()
+})
+	var Description = $("#description").val();
+	$(projectName + ".project-description").text(Description);
+}
+
+function projectClick(e) {
+	console.log("Project clicked")
+	//prevent the page from reloading
+	e.preventDefault();
+
+	//In an event handler, $(this) refers to
+	//the object that triggered the event
+	$(this).css("background-color", "#7fff00");	
+	var jumbotronHeader = $(".jumbotron h1");
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(containingProject).click(function(){
+       	$(".project-description").fadeOut();
+       });
+    }	 
 }
